f9427f52422c8a0fbd700bee4b91f2789beb5c9d
JuanAlvarez <jcaa01@gmail.com>
2014-10-30 09:57:37 -0600

    first

---
+++ pruevas.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+main()
+{
+double a;
+int i;
+for( i =0;i<10;i++){
+a= (double)rand()/RAND_MAX*M_PI;
+printf("%f\n", M_PI);
+}
+
+}
